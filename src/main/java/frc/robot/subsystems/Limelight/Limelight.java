7940b93c6f32d6dc7eff22a90f5d19673cb7af8d
baihu <baihu3210@gmail.com>
2026-02-02 12:16:37 +0800

    refactor: enhance Limelight class structure and add AprilTag target detection

--- src/main/java/frc/robot/subsystems/Limelight/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight/Limelight.java
@@ -1,17 +1,33 @@
 package frc.robot.subsystems.Limelight;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.PoseEstimate;
+import frc.robot.subsystems.Gyro.Gyro;
 
 public class Limelight {
-    private static final String limelightName = "limelight";
+    private static Limelight limelight;
+
+    private final String limelightName = "limelight";
+
+    Gyro gyro = Gyro.getInstance();
 
     public Limelight() {
         LimelightHelpers.setPipelineIndex(limelightName, 0);
         LimelightHelpers.setLEDMode_ForceOn(limelightName);
     }
 
-    public Pose2d getPose() {
-        return LimelightHelpers.getBotPose2d(limelightName);
+    public static Limelight getInstance() {
+        if (limelight == null) limelight = new Limelight();
+        return limelight;
+    }
+
+    public boolean hasAprilTagTarget() {
+        return LimelightHelpers.getTV(limelightName);
+    }
+
+    public PoseEstimate getMeasurement() {
+        LimelightHelpers.SetRobotOrientation(limelightName, gyro.getYaw(), 0, 0, 0, 0, 0);
+        PoseEstimate limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
+        return limelightMeasurement;
     }
 }
