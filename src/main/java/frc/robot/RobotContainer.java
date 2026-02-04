3aaca957811375659f340eb5db0e164b385c118c
baihu <baihu3210@gmail.com>
2026-02-04 16:03:42 +0800

    feat: enhance RobotContainer to integrate Intake subsystem and button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,20 +3,35 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.DriveCommands;
+import frc.robot.commands.IntakeCommands;
 import frc.robot.controls.DriverControls;
 import frc.robot.subsystems.Drivetrain.Drivetrain;
+import frc.robot.subsystems.Intake.Intake;
 
 public class RobotContainer {
-	private final Drivetrain drivetrain = Drivetrain.getInstance();
 	private final DriverControls controls = new DriverControls();
 
-	private boolean isFieldRelative = true;
+	private final Drivetrain drivetrain = Drivetrain.getInstance();
+	private final Intake intake = Intake.getInstance();
 
 	public RobotContainer() {
 		drivetrain.setDefaultCommand(
 			DriveCommands.defaultDrive(drivetrain, controls)
 		);
+
+		configureButtonBindings();
 	}
+
+	private void configureButtonBindings() {
+		controls.intakeButton().toggleOnTrue(
+			IntakeCommands.intakeCommand(intake)
+		);
+
+		controls.outtakeButton().toggleOnTrue(
+			IntakeCommands.outtakeCommand(intake)
+		);
+	}
+
 	public Command getAutonomousCommand() {
 		return Commands.print("No autonomous command configured!");
 	}
