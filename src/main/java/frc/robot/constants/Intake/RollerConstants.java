ba6164a354bdb90c6c078c972ad98cbfe9739c13
baihu <baihu3210@gmail.com>
2026-02-04 16:03:30 +0800

    feat: add RollerConstants class for roller motor configuration

---
+++ src/main/java/frc/robot/constants/Intake/RollerConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.constants.Intake;
+
+public class RollerConstants {
+    public static final int rollerMotorID = 31;
+
+    public static final double intakeSpeed = 0.7;
+    public static final double outtakeSpeed = -0.7;
+}
