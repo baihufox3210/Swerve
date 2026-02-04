9a42d37b029313e6f81c1e5d604cb6356ea0bdc2
baihu <baihu3210@gmail.com>
2026-02-04 12:27:37 +0800

    feat: refactor RobotContainer to use DriverControls for command input

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,51 +1,22 @@
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.DriveCommands;
-import frc.robot.constants.DrivetrainConstants;
+import frc.robot.controls.DriverControls;
 import frc.robot.subsystems.Drivetrain.Drivetrain;
 
 public class RobotContainer {
 	private final Drivetrain drivetrain = Drivetrain.getInstance();
-	private final Joystick joystick = new Joystick(0);
+	private final DriverControls controls = new DriverControls();
 
 	private boolean isFieldRelative = true;
 
 	public RobotContainer() {
 		drivetrain.setDefaultCommand(
-				DriveCommands.driveCommand(
-				drivetrain,
-				() -> getChassisSpeeds()
-			)
+			DriveCommands.defaultDrive(drivetrain, controls)
 		);
 	}
-
-	private ChassisSpeeds getChassisSpeeds() {
-		double xSpeed = -joystick.getY();
-		double ySpeed = -joystick.getX();
-		double rot = -joystick.getRawAxis(4);
-
-		if (isFieldRelative) {
-			return ChassisSpeeds.fromFieldRelativeSpeeds(
-					MathUtil.applyDeadband(xSpeed, 0.1) * DrivetrainConstants.maxSpeed,
-					MathUtil.applyDeadband(ySpeed, 0.1) * DrivetrainConstants.maxSpeed,
-					MathUtil.applyDeadband(rot, 0.1) * DrivetrainConstants.maxAngularSpeed,
-				drivetrain.getPose().getRotation()
-			);
-		} 
-		else {
-			return new ChassisSpeeds(
-					MathUtil.applyDeadband(xSpeed, 0.1) * DrivetrainConstants.maxSpeed,
-					MathUtil.applyDeadband(ySpeed, 0.1) * DrivetrainConstants.maxSpeed,
-				MathUtil.applyDeadband(rot, 0.1) * DrivetrainConstants.maxAngularSpeed
-			);
-		}
-	}
-
 	public Command getAutonomousCommand() {
 		return Commands.print("No autonomous command configured!");
 	}
