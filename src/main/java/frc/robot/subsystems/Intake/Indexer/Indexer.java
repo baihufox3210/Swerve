155ffc6b7a64bb7217dc625b3a1ce897cbdfd555
baihu <baihu3210@gmail.com>
2026-02-05 20:06:31 +0800

    feat: implement Indexer class with intake, outtake, and stop methods

--- src/main/java/frc/robot/subsystems/Intake/Indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/Intake/Indexer/Indexer.java
@@ -1,5 +1,30 @@
 package frc.robot.subsystems.Intake.Indexer;
 
+import frc.robot.constants.Intake.IndexerConstants;
+import frc.robot.subsystems.Intake.Indexer.module.IndexerModule;
+
 public class Indexer {
-    
+    private static Indexer indexer;
+    private final IndexerModule indexerModule;
+
+    public Indexer() {
+        indexerModule = new IndexerModule(IndexerConstants.indexerMotorID);
+    }
+
+    public void intake() {
+        indexerModule.setSpeed(IndexerConstants.intakeSpeed);
+    }
+
+    public void outtake() {
+        indexerModule.setSpeed(IndexerConstants.outtakeSpeed);
+    }
+
+    public void stop() {
+        indexerModule.setSpeed(0);
+    }
+
+    public static Indexer getInstance() {
+        if (indexer == null) indexer = new Indexer();
+        return indexer;
+    }
 }
