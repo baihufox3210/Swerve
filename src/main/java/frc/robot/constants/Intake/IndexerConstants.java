ecf62356306477f9db0231d8d013d3264d4f22ee
baihu <baihu3210@gmail.com>
2026-02-05 20:03:58 +0800

    feat: add IndexerConstants class with motor ID and speed configurations

---
+++ src/main/java/frc/robot/constants/Intake/IndexerConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.constants.Intake;
+
+public class IndexerConstants {
+    public static final int indexerMotorID = 31;
+
+    public static final double intakeSpeed = 0.7;
+    public static final double outtakeSpeed = -0.7;
+}
