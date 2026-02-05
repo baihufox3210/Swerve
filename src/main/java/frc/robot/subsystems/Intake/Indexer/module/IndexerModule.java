0ae1eff61e2369e7149d9a0b1e98a9aa366bc456
baihu <baihu3210@gmail.com>
2026-02-05 20:03:53 +0800

    feat: implement IndexerModule with motor configuration and speed control

--- src/main/java/frc/robot/subsystems/Intake/Indexer/module/IndexerModule.java
+++ src/main/java/frc/robot/subsystems/Intake/Indexer/module/IndexerModule.java
@@ -1,5 +1,21 @@
 package frc.robot.subsystems.Intake.Indexer.module;
 
+import com.revrobotics.spark.SparkMax;
+
 public class IndexerModule {
-    
+    private final SparkMax indexerMotor;
+
+    public IndexerModule(int indexerMotorID) {
+        indexerMotor = new SparkMax(indexerMotorID, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+
+        indexerMotor.configure(
+            IndexerConfigs.indexerConfig(),
+            com.revrobotics.ResetMode.kResetSafeParameters,
+            com.revrobotics.PersistMode.kNoPersistParameters
+        );
+    }
+
+    public void setSpeed(double speed) {
+        indexerMotor.set(speed);
+    }
 }
