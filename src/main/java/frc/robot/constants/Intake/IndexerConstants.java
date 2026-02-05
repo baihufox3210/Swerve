761563787b6cc7cf4da5a9a2b7dfc6a60169c51f
baihu <baihu3210@gmail.com>
2026-02-05 20:08:00 +0800

    fix: update indexerMotorID to correct value

--- src/main/java/frc/robot/constants/Intake/IndexerConstants.java
+++ src/main/java/frc/robot/constants/Intake/IndexerConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.constants.Intake;
 
 public class IndexerConstants {
-    public static final int indexerMotorID = 31;
+    public static final int indexerMotorID = 33;
 
     public static final double intakeSpeed = 0.7;
     public static final double outtakeSpeed = -0.7;
