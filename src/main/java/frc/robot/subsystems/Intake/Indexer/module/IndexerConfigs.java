871d4f0d2460a9fcead156343a12d115f8e14b64
baihu <baihu3210@gmail.com>
2026-02-05 19:59:51 +0800

    feat: implement indexer configuration with SparkMaxConfiguration setup

--- src/main/java/frc/robot/subsystems/Intake/Indexer/module/IndexerConfigs.java
+++ src/main/java/frc/robot/subsystems/Intake/Indexer/module/IndexerConfigs.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.Intake.Indexer.module;
 
+import frc.robot.utils.SparkMaxConfiguration;
+
 public class IndexerConfigs {
-    
+    public static SparkMaxConfiguration indexerConfig() {
+        return new SparkMaxConfiguration(40, false);
+    }
 }
