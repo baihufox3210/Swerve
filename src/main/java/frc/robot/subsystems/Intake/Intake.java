e66be465d88f1efc6bdf34d713a4575722ff07fa
baihu <baihu3210@gmail.com>
2026-02-05 20:07:55 +0800

    feat: integrate Indexer functionality into Intake subsystem

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,28 +1,32 @@
 package frc.robot.subsystems.Intake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Intake.Indexer.Indexer;
 import frc.robot.subsystems.Intake.Roller.Roller;
 
 public class Intake extends SubsystemBase {
     public static Intake intake;
 
     private final Roller roller = Roller.getInstance();
+    private final Indexer indexer = Indexer.getInstance();
 
     public void intake() {
         roller.intake();
+        indexer.intake();
     }
 
     public void outtake() {
         roller.outtake();
+        indexer.outtake();
     }
 
     public void stop() {
         roller.stop();
+        indexer.stop();
     }
 
     public static Intake getInstance() {
-        if (intake == null)
-            intake = new Intake();
+        if (intake == null) intake = new Intake();
         return intake;
     }
 }
