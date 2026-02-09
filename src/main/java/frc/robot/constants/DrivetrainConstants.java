099b14cc052b540b1b864abf15d3a92cb517f609
baihu-frc <baihu3210@gmail.com>
2026-02-09 20:06:52 +0800

    feat: Implement initial robot structure, drivetrain constants, and shooter command.

---
+++ src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -0,0 +1,55 @@
+package frc.robot.constants;
+
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
+public class DrivetrainConstants { 
+    public static final int drivingMotorID[] = {11, 12, 13, 14};
+    public static final int turningMotorID[] = {21, 22, 23, 24};
+    public static final int shooterMotorID[] = {41,42};
+
+    public static final double MaxSpeedMetersPerSecond = 4.5;
+
+    public static final double DriveGearRatio = 5.08;
+    public static final double WheelCirc = Inches.of(3).times(Math.PI).in(Meters);
+
+    public static final double PositionConversionFactor = 1 / DriveGearRatio * WheelCirc;
+    public static final double VelocityConversionFactor = PositionConversionFactor / 60;
+
+    public static final double maxSpeed = 5676 * VelocityConversionFactor;
+
+    public static final double TurnGearRatio = 25.0;
+
+    public static final double TurnPositionConversionFactor = 2 * Math.PI;
+    public static final double TurnVelocityConversionFactor = TurnPositionConversionFactor / 60;
+
+    public static final double maxAngularSpeed = 11000 / TurnGearRatio * TurnVelocityConversionFactor;
+
+    public static final double[] AngleOffsetRadiants = {
+        0.6340491,
+        0.8244288,
+        0.5092767,
+        0.6858476
+    };
+
+    public static final double TrackWidth = 0.635;
+    public static final double WheelBase = 0.635;
+
+    public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+        new Translation2d(WheelBase / 2, TrackWidth / 2),
+        new Translation2d(WheelBase / 2, -TrackWidth / 2),
+        new Translation2d(-WheelBase / 2, TrackWidth / 2),
+        new Translation2d(-WheelBase / 2, -TrackWidth / 2)
+    );
+
+    public static final Pose2d InitialPose = new Pose2d(0, 0, Rotation2d.kZero);
+
+    public static final IdleMode MotorMode = IdleMode.kBrake;
+}
